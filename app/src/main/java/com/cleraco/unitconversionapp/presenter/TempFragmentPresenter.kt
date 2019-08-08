be9d718d2c533199456cbbf42dc194b22368c4c1
Ashit Rathore <asht88.rathore@gmail.com>
2019-08-08 16:07:14 +1200

    Added changes for Delisle Unit handling

--- app/src/main/java/com/cleraco/unitconversionapp/presenter/TempFragmentPresenter.kt
+++ app/src/main/java/com/cleraco/unitconversionapp/presenter/TempFragmentPresenter.kt
@@ -3,7 +3,6 @@ package com.cleraco.unitconversionapp.presenter
 import com.cleraco.unitconversionapp.data.Converter
 import com.cleraco.unitconversionapp.data.ConverterImpl
 import com.cleraco.unitconversionapp.ui.TempConversionFragment
-import javax.measure.converter.UnitConverter
 import javax.measure.quantity.Temperature
 import javax.measure.unit.NonSI
 import javax.measure.unit.SI
@@ -12,26 +11,29 @@ import javax.measure.unit.Unit
 class TempFragmentPresenter(
     private val view : TempConversionFragment
 ) : BasePresenter{
-    private lateinit var converter: Converter
+    private var converter: Converter
     init {
         converter = ConverterImpl()
+
+
     }
     override fun calculateValue(intUnit1 : Int?, intUnit2 : Int?, inputValue : String){
 
         val inputUnit : Unit<Temperature>?  = getUnitType(intUnit1)
         val outputUnit : Unit<Temperature>? = getUnitType(intUnit2)
 
-        var result = converter.convertTemp(inputUnit!!,outputUnit!!,inputValue);
+        val result = converter.convertTemp(inputUnit!!,outputUnit!!,inputValue)
 
         view.conversionResult("${result}")
     }
 
     private fun getUnitType(pos: Int?) : Unit<Temperature>?{
 
-        var tempUnit = when (pos) {
+        val tempUnit = when (pos) {
             0 -> SI.KELVIN
             1 -> SI.CELSIUS
             2 -> NonSI.FAHRENHEIT
+            3 -> (SI.CELSIUS.plus(100.0).times(2.0)).divide(3.0).times(-1)
             else -> null
             }
 
